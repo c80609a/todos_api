96c867c2c2d209cff84a0644fcebef5220d484c1
C80609A <c080609a@gmail.com>
2017-05-30 06:58:58 +0300

    Добавил код в модуль ExceptionHanlder, который спасает от исклю- чительных ситуаций с помощью внутренних подклассов StandardError.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,14 +1,35 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # определим кастомные error subclasses - rescue catches `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+
+    # define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({message: e.message}, :not_found)
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({message: e.message}, :unprocessable_entity)
-    end
+  end
+
+  private
+  
+  # JSON ответ со статус-кодом 422 - unprocessable_entity
+  def four_twenty_two(e)
+    json_response({message: e.message}, :unprocessable_entity)
+  end
+
+  # JSON ответ со статус-кодом 401 - unauthorized
+  def unauthorized_request(e)
+    json_response({message: e.message}, :unauthorized)
   end
 
 end
