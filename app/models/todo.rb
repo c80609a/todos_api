ea569e4d34a965171fdc0e5608fbd27265fb2ae4
C80609A <c080609a@gmail.com>
2017-05-30 11:59:35 +0300

    Добавил опцию `:optional => true` в ассоциацию `belongs_to` класса Todo.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ApplicationRecord
   has_many :items, :dependent => :destroy
-  belongs_to :user
+  belongs_to :user, :optional => true
   validates_presence_of :title, :created_by
 end
